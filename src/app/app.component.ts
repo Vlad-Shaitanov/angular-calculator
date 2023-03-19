e3c10f98d6cedc55b076423882412bbe5e9d748c
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2023-03-19 15:09:36 +0300

    feat: Добавлена обработка ввода цифр

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+
+  calcValue: number = 0;
+  funcT: any = "NoFunction";
+  calcNumber: string = "noValue";
+
+  onClickValue(val: string, type: any){
+    if(type === "number"){
+      this.onNumberClick(val);
+    }
+  }
+
+  onNumberClick(val: string){
+    if(this.calcNumber !== "noValue"){
+      this.calcNumber = this.calcNumber + val;
+    } else {
+      this.calcNumber = val;
+    }
+
+    this.calcValue = parseFloat(this.calcNumber);
+  }
 }

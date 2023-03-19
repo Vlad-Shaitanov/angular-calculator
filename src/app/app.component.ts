72953f42ce5441a9d05232bcdf4d1c322c5ad7f7
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2023-03-19 15:59:38 +0300

    feat: Добавлена обработка функций

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,15 @@ export class AppComponent {
   calcValue: number = 0;
   funcT: any = "NoFunction";
   calcNumber: string = "noValue";
+  firstNumber: number = 0;
+  secondNumber: number = 0;
 
   onClickValue(val: string, type: any){
     if(type === "number"){
       this.onNumberClick(val);
+    }
+    if(type === "function"){
+      this.onFunctionClick(val);
     }
   }
 
@@ -26,5 +31,73 @@ export class AppComponent {
     }
 
     this.calcValue = parseFloat(this.calcNumber);
+  }
+
+  onFunctionClick(val: string){
+
+    if(this.funcT === "NoFunction"){
+      this.firstNumber = this.calcValue;
+      this.calcValue = 0;
+      this.calcNumber = "noValue";
+      this.funcT = val;
+    }else if(this.funcT !== "NoFunction"){
+      this.secondNumber = this.calcValue;
+      this.valueCalculate(val);
+    }
+    if(val === "c"){
+      this.clearAll();
+    }
+  }
+
+  valueCalculate(val: string){
+    if(this.funcT === '+'){
+      const Total = this.firstNumber + this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+
+    if(this.funcT === '-'){
+      const Total = this.firstNumber - this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+
+    if(this.funcT === '*'){
+      const Total = this.firstNumber * this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+
+    if(this.funcT === '/'){
+      const Total = this.firstNumber / this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+
+    if(this.funcT === '%'){
+      const Total = this.firstNumber % this.secondNumber;
+      this.totalAssignValues(Total, val);
+    }
+  }
+
+  totalAssignValues(Total: number, val: string){
+    this.calcValue = Total;
+    this.firstNumber = Total;
+    this.secondNumber = 0;
+    this.calcNumber = "noValue";
+    this.funcT = val;
+
+    if(val === "="){this.onEqualPress()}
+  }
+
+  onEqualPress(){
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = "NoFunction";
+    this.calcNumber = "noValue";
+  }
+
+  clearAll(){
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.calcValue = 0;
+    this.funcT = "NoFunction";
+    this.calcNumber = "noValue";
   }
 }
